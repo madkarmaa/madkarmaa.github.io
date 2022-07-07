1ecebc879153b4fdaf89298492f4cd472e3c912f
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-07-07 19:49:04 +0200

    Commands

--- bongocat/js/main.js
+++ bongocat/js/main.js
@@ -1,6 +1,6 @@
 var myVar;
 
-function myFunction() {
+function loadingAnimation() {
     myVar = setTimeout(showPage, 3000);
 }
 
@@ -10,3 +10,45 @@ function showPage() {
     document.getElementById("app").style.display = "flex";
     document.getElementById("title").style.display = "block";
 }
+
+$(document).ready(function(){
+    $("#general").click(function(){
+        $(".commands:not(#general-commands)").hide();
+        $("#general-commands").toggle(500);
+    });
+});
+
+$(document).ready(function(){
+    $("#welcomer").click(function(){
+        $(".commands:not(#welcomer-commands)").hide();
+        $("#welcomer-commands").toggle(500);
+    });
+});
+
+$(document).ready(function(){
+    $("#economy").click(function(){
+        $(".commands:not(#economy-commands)").hide();
+        $("#economy-commands").toggle(500);
+    });
+});
+
+$(document).ready(function(){
+    $("#gifs").click(function(){
+        $(".commands:not(#gifs-commands)").hide();
+        $("#gifs-commands").toggle(500);
+    });
+});
+
+$(document).ready(function(){
+    $("#meters").click(function(){
+        $(".commands:not(#meters-commands)").hide();
+        $("#meters-commands").toggle(500);
+    });
+});
+
+$(document).ready(function(){
+    $("#games").click(function(){
+        $(".commands:not(#games-commands)").hide();
+        $("#games-commands").toggle(500);
+    });
+});
