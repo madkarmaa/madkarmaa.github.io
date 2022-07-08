b67df91d474f86bded113a762c8e037caa7ad907
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-07-08 19:55:58 +0200

    update

--- bongocat/js/main.js
+++ bongocat/js/main.js
@@ -1,12 +1,6 @@
-$(document).ready(function() {
-    var movementStrength = 25;
-    var height = movementStrength / $(window).height();
-    var width = movementStrength / $(window).width();
-    $("body").mousemove(function(e){
-        var pageX = e.pageX - ($(window).width() / 2);
-        var pageY = e.pageY - ($(window).height() / 2);
-        var newvalueX = width * pageX * -1 - 25;
-        var newvalueY = height * pageY * -1 - 50;
-        $('body').css("background-position", newvalueX+"px     "+newvalueY+"px");
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
 });
