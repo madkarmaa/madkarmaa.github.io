51210e0f0779aa4f095f7c76098400c7ebe23d02
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-07-10 19:36:43 +0200

    Invite button

--- bongocat/js/main.js
+++ bongocat/js/main.js
@@ -4,3 +4,7 @@ $(function() {
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
     });
 });
+
+document.getElementById("invite").onclick = function() {
+    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=941300861785739264&permissions=8&scope=bot"
+};
