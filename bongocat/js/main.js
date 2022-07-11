954cabbbadc98c5ac8e3a077c50b6e1e257c7757
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-07-11 15:43:31 +0200

    update section 2

--- bongocat/js/main.js
+++ bongocat/js/main.js
@@ -1,10 +1,17 @@
-$(function() {
-    $('a[href*="#"]').on('click', function(e) {
-        e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
+$(function () {
+  $('a[href*="#"]').on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      500,
+      "linear"
+    );
+  });
 });
 
-document.getElementById("invite").onclick = function() {
-    window.location.href = "https://discord.com/api/oauth2/authorize?client_id=941300861785739264&permissions=8&scope=bot"
+document.getElementById("invite").onclick = function () {
+  window.open(
+    "https://discord.com/api/oauth2/authorize?client_id=941300861785739264&permissions=8&scope=bot",
+    "_blank"
+  );
 };
