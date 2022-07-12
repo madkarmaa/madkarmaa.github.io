6b080d9344c0e15aa6c213b6dc205524273bbd96
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2022-07-12 14:02:52 +0200

    progress bar

--- bongocat/js/main.js
+++ bongocat/js/main.js
@@ -15,3 +15,16 @@ document.getElementById("invite").onclick = function () {
     "_blank"
   );
 };
+
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
