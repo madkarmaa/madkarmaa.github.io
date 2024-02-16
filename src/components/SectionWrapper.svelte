d3b2c68d0976b2bd8939636fda472d51475c43dd
madkarmaa <madkarma99@gmail.com>
2024-02-16 11:21:33 +0100

    feat: added tailwind css support

---
+++ src/components/SectionWrapper.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let id;
+</script>
+
+<section {id}>
+	<div>
+		<slot />
+	</div>
+</section>
---
+++ src/routes/revanced-interactive-tutorial/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	// your script goes here
+</script>
+
+<div class="container bgGrid" />
+
+<style>
+	.bgGrid {
+		background-size: 40px 40px;
+		background-image: linear-gradient(to right, grey 1px, transparent 1px),
+			linear-gradient(to bottom, grey 1px, transparent 1px);
+	}
+
+	.container {
+		flex: 1;
+	}
+</style>
