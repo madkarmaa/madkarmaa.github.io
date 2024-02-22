34ccb9367d86306765bfc572d3fb24c055d3f86a
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-02-22 17:03:19 +0100

    fix: page still dynamic

--- src/routes/rnd/+page.svelte
+++ src/routes/rnd/+page.svelte
@@ -1,14 +1,13 @@
 <script>
 	import { getRandomNumber } from '../../lib/random.js';
 	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
 
-	let searchParams = $page.url.searchParams;
-	let from = parseInt(searchParams.get('from')) || 0;
-	let to = parseInt(searchParams.get('to')) || 0;
 	let rndNumber;
 
 	onMount(() => {
+		let searchParams = new URLSearchParams(window.location.href);
+		let from = parseInt(searchParams.get('from')) || 0;
+		let to = parseInt(searchParams.get('to')) || 0;
 		rndNumber = getRandomNumber(from, to);
 	});
 </script>
