c512f1a1ceede487f942085a1b995f946e9f5fe7
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-02-22 17:53:19 +0100

    feat: show img on 69

--- src/routes/rnd/+page.svelte
+++ src/routes/rnd/+page.svelte
@@ -1,14 +1,24 @@
 <script>
 	import { getRandomNumber } from '../../lib/random.js';
 	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 
 	let rndNumber;
+
+	let show = false;
 
 	onMount(() => {
 		let searchParams = new URLSearchParams(window.location.search);
 		let from = parseInt(searchParams.get('from')) || 0;
 		let to = parseInt(searchParams.get('to')) || 0;
 		rndNumber = getRandomNumber(from, to);
+
+		if (rndNumber === 69) {
+			show = true;
+			setTimeout(() => {
+				show = false;
+			}, 300);
+		}
 	});
 </script>
 
@@ -18,4 +28,7 @@
 			{rndNumber}
 		{/if}
 	</span>
+	{#if show}
+		<img transition:fade={{ duration: 300 }} class="absolute" src="/blush.png" alt="blush" />
+	{/if}
 </div>
