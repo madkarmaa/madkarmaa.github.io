d3c65f21893ab140e56de0396bea36b2141a1728
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-03-08 18:20:18 +0100

    feat: example cards

---
+++ src/lib/rvSteps.ts
@@ -0,0 +1,33 @@
+interface Step {
+    title: string;
+    description: string;
+    image_path?: string;
+}
+
+export const STEPS: Step[] = [
+    {
+        title: 'balls',
+        description: 'guh',
+        image_path: 'https://picsum.photos/1920/1080',
+    },
+    {
+        title: 'balls',
+        description: 'guh',
+        image_path: 'https://picsum.photos/1920/1080',
+    },
+    {
+        title: 'balls',
+        description: 'guh',
+        image_path: 'https://picsum.photos/1920/1080',
+    },
+    {
+        title: 'balls',
+        description: 'guh',
+        image_path: 'https://picsum.photos/1920/1080',
+    },
+    {
+        title: 'balls',
+        description: 'guh',
+        image_path: 'https://picsum.photos/1920/1080',
+    },
+];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import * as Carousel from "$lib/components/ui/carousel/index.js";
+  import * as Card from "$lib/components/ui/card/index.js";
+  import { AspectRatio } from "$lib/components/ui/aspect-ratio";
+  import { STEPS } from "@/rvSteps";
+</script>
+
+<div class="container flex justify-center items-center h-screen w-full p-10">
+
+  <Carousel.Root class="w-full h-full">
+    <Carousel.Content>
+
+      {#each Array(STEPS.length) as _, i (i)}
+        <Carousel.Item>
+
+          <Card.Root class="w-full h-full">
+
+            <Card.Header>
+              <Card.Title>{STEPS[i].title}</Card.Title>
+              <Card.Description>{STEPS[i].description}</Card.Description>
+            </Card.Header>
+
+            {#if STEPS[i].image_path}
+              <Card.Content class="grid gap-4">
+                <AspectRatio ratio={16 / 9} class="bg-muted">
+                  <img src={STEPS[i].image_path} alt={STEPS[i].title} class="rounded-md">
+                </AspectRatio>
+              </Card.Content>
+            {/if}
+
+            <Card.Footer>
+              <small>Step {i + 1}</small>
+            </Card.Footer>
+          </Card.Root>
+
+        </Carousel.Item>
+  	  {/each}
+
+    </Carousel.Content>
+
+    <Carousel.Previous />
+    <Carousel.Next />
+
+  </Carousel.Root>
+
+</div>
