91fa8526cf2866c04489eaec2d788096dec89aa7
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-03-10 20:28:48 +0100

    refactor: remove braces from inline arrow function

--- src/routes/revanced/tutorial/+page.svelte
+++ src/routes/revanced/tutorial/+page.svelte
@@ -52,7 +52,7 @@
               {/if}
 
               <Card.Footer class="px-4 py-2 flex items-center justify-end">
-                <Button on:click={() => { copyToClipboard(window.location.origin + window.location.pathname + '?step=' + (i + 1)) }}>
+                <Button on:click={() => copyToClipboard(window.location.origin + window.location.pathname + '?step=' + (i + 1)) }>
                   <FAIcon iconName="link-simple" type={FAIconType.REGULAR}></FAIcon>
                   Step {i + 1}
                 </Button>
