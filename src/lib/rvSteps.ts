28161f903d039ad5aa8cc16d5966b2e9a5e4f9fa
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-03-10 20:14:46 +0100

    ci: moved code to different route

--- src/lib/rvSteps.ts
+++ src/lib/rvSteps.ts
@@ -25,4 +25,9 @@ export const STEPS: Step[] = [
         title: 'balls',
         description: 'guh',
     },
+    {
+        title: "Still can't understand?",
+        description:
+            'Watch a video tutorial <a href="#" target="_blank" class="text-primary hover:underline font-semibold">here</a>',
+    },
 ];
