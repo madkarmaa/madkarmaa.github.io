9841eb1b0186e7c4efcd75a1e5a15724b87466cf
madkarmaa <madkarma99@gmail.com>
2024-03-11 13:03:07 +0100

    feat: add tutorial steps

--- src/lib/rvTutorialSteps.ts
+++ src/lib/rvTutorialSteps.ts
@@ -6,28 +6,55 @@ interface Step {
 
 export const STEPS: Step[] = [
     {
-        title: 'balls',
-        description: 'guh',
+        title: 'Download the manager',
+        description:
+            'Go to <a href="https://revanced.app/download" target="_blank">the official website</a>, download and install the manager.',
     },
     {
-        title: 'balls',
-        description: 'guh',
+        title: 'REMEMBER!!!',
+        description:
+            'The official website is ONLY <a href="https://revanced.app" target="_blank">revanced.app</a>. Any other counterfeit website such as (<i>but not limited to</i>) <b>revanced.net</b> are fake and are most probably distributing malware.',
     },
     {
-        title: 'balls',
-        description: 'guh',
+        title: 'Find the app you want to patch',
+        description: 'Open the <b>manager > Patcher > Select an app > Search app</b>.',
     },
     {
-        title: 'balls',
-        description: 'guh',
+        title: 'Search the app on the browser',
+        description:
+            "After you've found the app you want to patch, click on the button that says <b>Suggested</b> and open the first search result from the website <b>APKMirror</b>.",
     },
     {
-        title: 'balls',
-        description: 'guh',
+        title: 'Download the apk of the app',
+        description:
+            'Make sure the title of the app states <b>nodpi</b>, then click on the <b>Download APK</b> button.',
+    },
+    {
+        title: 'Select the apk in the manager',
+        description:
+            'Go back to the <b>manager app > Patcher > Select an app > Storage</b> (the button in the lower right corner).',
+    },
+    {
+        title: 'Select the default set of patches',
+        description:
+            'On the patcher screen, click on the <b>Selected patches</b> box, then click on the <b>Default</b> button at the top of the screen, then click on the <b>Done</b> button (lower right corner).',
+    },
+    {
+        title: 'Want to customize the patches selection?',
+        description:
+            "Maybe the default set of patches isn't enough for you, so go ahead and select/deselect the patches you want!",
+    },
+    {
+        title: 'Patch the app',
+        description: 'Click on the <b>Patch</b> button in the lower right corner of the screen, and... just wait :P',
+    },
+    {
+        title: 'Install the app',
+        description:
+            "Once the patching process is complete, click on the <b>Install</b> button, and you're good to go!",
     },
     {
         title: "Still can't understand?",
-        description:
-            'Watch a video tutorial <a href="#" target="_blank" class="text-primary hover:underline font-semibold">here</a>',
+        description: 'Watch a video tutorial <a href="#" target="_blank">here</a>',
     },
 ];
