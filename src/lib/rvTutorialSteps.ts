2c34acc852d68927dd19e0448c004a1df4178db4
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-03-11 17:37:48 +0100

    refactor: better wording

--- src/lib/rvTutorialSteps.ts
+++ src/lib/rvTutorialSteps.ts
@@ -8,16 +8,16 @@ export const STEPS: Step[] = [
     {
         title: 'Download the manager',
         description:
-            'Go to <a href="https://revanced.app/download" target="_blank">the official website</a>, download and install the manager.',
+            'Go to <a href="https://revanced.app/download" target="_blank">the official website</a>, download and install the ReVanced Manager.',
     },
     {
-        title: 'REMEMBER!!!',
+        title: 'BE AWARE!!!',
         description:
-            'The official website is ONLY <a href="https://revanced.app" target="_blank">revanced.app</a>. Any other counterfeit website such as (<i>but not limited to</i>) <b>revanced.net</b> are fake and are most probably distributing malware.',
+            'The official website is <b>ONLY</b> <a href="https://revanced.app" target="_blank">revanced.app</a>. Any other counterfeit website such as (<i>but not limited to</i>) <b>revanced.net</b> is fake and is most probably distributing malware.',
     },
     {
         title: 'Find the app you want to patch',
-        description: 'Open the <b>manager > Patcher > Select an app > Search app</b>.',
+        description: 'Open the <b>ReVanced Manager > Patcher > Select an app > Search app</b>.',
     },
     {
         title: 'Search the app on the browser',
@@ -32,12 +32,12 @@ export const STEPS: Step[] = [
     {
         title: 'Select the apk in the manager',
         description:
-            'Go back to the <b>manager app > Patcher > Select an app > Storage</b> (the button in the lower right corner).',
+            'Go back to the <b>ReVanced Manager > Patcher > Select an app > Storage</b> (the button in the lower right corner).',
     },
     {
         title: 'Select the default set of patches',
         description:
-            'On the patcher screen, click on the <b>Selected patches</b> box, then click on the <b>Default</b> button at the top of the screen, then click on the <b>Done</b> button (lower right corner).',
+            'On the patcher screen, click on the <b>Selected patches</b> box, then click on the <b>Default</b> button at the top of the screen, then click on <b>Done</b> (lower right corner).',
     },
     {
         title: 'Want to customize the patches selection?',
@@ -51,10 +51,10 @@ export const STEPS: Step[] = [
     {
         title: 'Install the app',
         description:
-            "Once the patching process is complete, click on the <b>Install</b> button, and you're good to go!",
+            "Once the patching process is complete, click on the <b>Install</b> button in the lower right corner, and you're good to go!",
     },
     {
         title: "Still can't understand?",
-        description: 'Watch a video tutorial <a href="#" target="_blank">here</a>',
+        description: 'Watch a video tutorial <a href="#" target="_blank">here</a>.',
     },
 ];
