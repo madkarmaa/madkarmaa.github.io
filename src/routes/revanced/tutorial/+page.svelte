7a040d1e507d71db8bca446a9ff0f826f24a9090
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-03-11 17:08:17 +0100

    feat: add progress bar

--- src/routes/revanced/tutorial/+page.svelte
+++ src/routes/revanced/tutorial/+page.svelte
@@ -2,10 +2,11 @@
     import { onMount } from 'svelte';
 
     import * as Carousel from '$lib/components/ui/carousel/index.js';
+    import { type CarouselAPI } from '$lib/components/ui/carousel/context.js';
     import * as Card from '$lib/components/ui/card/index.js';
-    import { AspectRatio } from '$lib/components/ui/aspect-ratio';
     import { Separator } from '$lib/components/ui/separator';
     import { Button } from '$lib/components/ui/button';
+    import { Progress } from '$lib/components/ui/progress';
     import FAIcon from '@/components/FAIcon.svelte';
 
     import { STEPS } from '@/rvTutorialSteps';
@@ -15,23 +16,39 @@
     const share = (step: number) =>
         copyToClipboard(`${window.location.origin}${window.location.pathname}?step=${step}`);
 
-    let index = 0;
+    const getPercentage = (max: number, step: number) => (100 * step) / max;
+
+    let value: number = 0;
+    let api: CarouselAPI;
+    let count: number = 0;
+    let current: number = 0;
+    let searchParams: URLSearchParams;
+
+    $: if (api) {
+        count = api.scrollSnapList().length;
+
+        value = getPercentage(count, current);
+        api.scrollTo(current - 1, true);
+
+        api.on('select', () => {
+            current = api.selectedScrollSnap() + 1;
+            value = getPercentage(count, current);
+        });
+    }
 
     onMount(() => {
-        const searchParams = new URLSearchParams(window.location.search);
-        index = parseInt(searchParams.get('step') || '1') - 1;
+        searchParams = new URLSearchParams(window.location.search);
 
-        if (index < 0) index = 0;
-        else if (index > STEPS.length) index = STEPS.length - 1;
+        current = parseInt(searchParams.get('step') || '1');
+
+        if (current < 0) current = 0;
+        else if (current > STEPS.length) current = STEPS.length;
     });
 </script>
 
 <main class="flex min-h-screen justify-center items-center">
     <div class="container mx-auto px-4 py-8 flex flex-col items-center">
-        <Carousel.Root
-            class="w-full max-w-3xl rounded-lg overflow-hidden md:overflow-visible"
-            opts={{ startIndex: index }}
-        >
+        <Carousel.Root class="w-full max-w-3xl rounded-lg overflow-hidden md:overflow-visible" bind:api>
             <Carousel.Content>
                 {#each STEPS as step, i (i)}
                     <Carousel.Item class="flex flex-col space-y-4">
@@ -47,13 +64,7 @@
 
                             {#if step.image_path}
                                 <Card.Content class="flex-grow overflow-hidden">
-                                    <AspectRatio ratio={9 / 16} class="w-full rounded-t-lg bg-transparent">
-                                        <img
-                                            src={step.image_path}
-                                            alt={step.title}
-                                            class="object-cover w-full h-full"
-                                        />
-                                    </AspectRatio>
+                                    <img src={step.image_path} alt={step.title} class="object-cover w-full h-full" />
                                 </Card.Content>
                             {/if}
 
@@ -68,8 +79,10 @@
                 {/each}
             </Carousel.Content>
 
-            <Carousel.Previous class="size-10" />
-            <Carousel.Next class="size-10" />
+            <Carousel.Previous class="size-10 hover:bg-primary" />
+            <Carousel.Next class="size-10 hover:bg-primary" />
         </Carousel.Root>
+
+        <Progress {value} />
     </div>
 </main>
