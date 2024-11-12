962a1fe1d9e5a236a8db856fe3c75b5129278d94
madkarmaa <madkarma99@gmail.com>
2024-11-12 16:55:31 +0100

    feat: add log message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,7 @@
 			Object.defineProperty(window, 'OMG_CARRRRRR', {
 				get: () => {
 					car.set(!$car);
+					if ($car) console.log('%cmeow meow 😼', 'font-size: 3rem; color: #f0f;');
 				}
 			});
 		} catch {}
