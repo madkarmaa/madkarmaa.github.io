ce376767135ac84962ff8ef96abf1345537a2b7e
madkarmaa <madkarma99@gmail.com>
2024-12-17 18:13:01 +0100

    feat: add minToMs util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,7 @@
 export const secToMs = (sec: number): number => sec * 1000;
 
+export const minToMs = (min: number): number => secToMs(min * 60);
+
 export const choose = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)];
 
 export const base64 = {
