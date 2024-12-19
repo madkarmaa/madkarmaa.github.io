895fd32df20f9704ed8202391e6132b0f7a23713
madkarmaa <madkarma99@gmail.com>
2024-12-19 15:24:00 +0100

    refactor: rename param

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,7 @@ export const secToMs = (sec: number): number => sec * 1000;
 
 export const minToMs = (min: number): number => secToMs(min * 60);
 
-export const choose = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)];
+export const choose = <T>(array: T[]): T => array[Math.floor(Math.random() * array.length)];
 
 export const base64 = {
 	encode: (input: string): string => btoa(input),
