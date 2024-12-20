e3214adada622a974006dc80959e4a9cd2a84b69
madkarmaa <madkarma99@gmail.com>
2024-12-20 16:45:46 +0100

    refactor: remove try-catch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,12 @@
 	import Neko from '@/components/Neko.svelte';
 
 	onMount(() => {
-		try {
-			Object.defineProperty(window, 'OMG_CARRRRRR', {
-				get: () => {
-					car.set(!$car);
-					if ($car) console.log('%cmeow meow 😼', 'font-size: 3rem; color: #f0f;');
-				}
-			});
-		} catch {}
+		Object.defineProperty(window, 'OMG_CARRRRRR', {
+			get: () => {
+				car.set(!$car);
+				if ($car) console.log('%cmeow meow 😼', 'font-size: 3rem; color: #f0f;');
+			}
+		});
 	});
 </script>
 
