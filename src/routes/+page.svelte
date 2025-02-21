9064569401625717ee64ae593cd09511ff4ab0db
madkarmaa <madkarma99@gmail.com>
2025-02-21 13:13:17 +0100

    feat: add heading

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Heading from './Heading.svelte';
+</script>
+
+<Heading />
---
+++ src/routes/Heading.svelte
@@ -0,0 +1,16 @@
+<h1 id="heading" class="noselect">MadKarma</h1>
+
+<style>
+	#heading {
+		font-size: 10rem;
+		color: var(--text);
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		min-height: 100vh;
+
+		mix-blend-mode: color-dodge;
+	}
+</style>
