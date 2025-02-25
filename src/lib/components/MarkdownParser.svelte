fe2d01d8ae3e8a8c3a22f4f48d6975e1654ebb87
madkarmaa <madkarma99@gmail.com>
2025-02-25 18:50:47 +0100

    feat: add MarkdownParser component
    TODO: sanitization

---
+++ src/lib/components/MarkdownParser.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { marked as parse } from 'marked';
+
+	type Props = { input: string };
+	let { input = $bindable('') }: Props = $props();
+
+	$effect(() => {
+		// https://marked.js.org/#usage
+		input = input.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, '').trim();
+	});
+</script>
+
+<div class="markdown-container">
+	{@html parse(input, { async: false })}
+</div>
+
+<style>
+</style>
