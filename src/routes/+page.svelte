c5e8e66919f03008522bc7aad2e3e47f2caa3c5d
madkarmaa <madkarma99@gmail.com>
2025-02-25 10:43:01 +0100

    feat: add example sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,46 @@
 </script>
 
 <Heading text="MadKarma" />
+
+<Section.Container>
+	<Section.Item>
+		{#snippet image()}
+			<img alt="sample" src="https://picsum.photos/1920/1080" />
+		{/snippet}
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ducimus quo repellendus voluptatum
+		ullam fugiat nihil reiciendis consectetur minima molestiae nam id perferendis neque, ab placeat ad
+		alias, recusandae labore?
+	</Section.Item>
+	<Section.Item>
+		{#snippet image()}
+			<img alt="sample" src="https://picsum.photos/1920/1080" />
+		{/snippet}
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ducimus quo repellendus voluptatum
+		ullam fugiat nihil reiciendis consectetur minima molestiae nam id perferendis neque, ab placeat ad
+		alias, recusandae labore?
+	</Section.Item>
+	<Section.Item>
+		{#snippet image()}
+			<img alt="sample" src="https://picsum.photos/1920/1080" />
+		{/snippet}
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ducimus quo repellendus voluptatum
+		ullam fugiat nihil reiciendis consectetur minima molestiae nam id perferendis neque, ab placeat ad
+		alias, recusandae labore?
+	</Section.Item>
+	<Section.Item>
+		{#snippet image()}
+			<img alt="sample" src="https://picsum.photos/1920/1080" />
+		{/snippet}
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ducimus quo repellendus voluptatum
+		ullam fugiat nihil reiciendis consectetur minima molestiae nam id perferendis neque, ab placeat ad
+		alias, recusandae labore?
+	</Section.Item>
+	<Section.Item>
+		{#snippet image()}
+			<img alt="sample" src="https://picsum.photos/1920/1080" />
+		{/snippet}
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ducimus quo repellendus voluptatum
+		ullam fugiat nihil reiciendis consectetur minima molestiae nam id perferendis neque, ab placeat ad
+		alias, recusandae labore?
+	</Section.Item>
+</Section.Container>
